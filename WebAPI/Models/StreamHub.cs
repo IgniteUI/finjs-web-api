9b885f1756cacfe4f2785105fbceccea5eec2f2e
Zdravko Kolev <ZKolev@infragistics.com>
2021-03-04 14:56:11 +0200

    Bind both volume and frequency sliders to signalr hub

--- WebAPI/Models/StreamHub.cs
+++ WebAPI/Models/StreamHub.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.SignalR;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Channels;
 using System.Threading.Tasks;
@@ -9,7 +11,43 @@ namespace WebAPI.Models
 {
     public class StreamHub: Hub
     {
-        public async Task BroadcastData(FinancialData[] data) =>
-            await Clients.All.SendAsync("broadcastdata", data);
+        public TimerManager timerManager;
+        private Helper helper = new Helper();
+
+        public StreamHub()
+        {
+
+        }
+        public void UpdateParameters(int ms, int volume)
+        {
+            StopTimer();
+
+            FinancialData[] dataArray = JsonConvert.DeserializeObject<List<FinancialData>>(helper.jsonData).ToArray();
+            FinancialData[] newDataArray = new FinancialData[volume];
+            newDataArray = helper.generatedata(dataArray, volume);
+
+            Debug.WriteLine("Ms: " + ms + ", Volume:" + volume);
+            Send(newDataArray);
+
+            // With the sendasync expression we are sending the data to all clients subscribed to transferdata event.
+            // Every client that has a listener to this event will receive the data.
+            //timerManager = new TimerManager(() =>
+            //{
+            //    helper.updateAllPrices(newDataArray);
+
+            //    Send(newDataArray);
+            //}, ms);
+        }
+        public async Task Send(FinancialData[] array)
+        {
+            Debug.WriteLine("Items count: " + array.Length);
+
+            await Clients.Caller.SendAsync("transferdata", array);
+        }
+
+        public void StopTimer()
+        {
+            if (timerManager != null) { timerManager.Stop(); }
+        }
     }
 }
--- WebAPI/Models/TimerManager.cs
+++ WebAPI/Models/TimerManager.cs
@@ -14,22 +14,34 @@ namespace WebAPI.Models
 
         public DateTime TimerStarted { get; set; }
 
-        public TimerManager(Action action)
+        public TimerManager(Action action, int milliseconds = 100)
         {
             _action = action;
             _autoResetEvent = new AutoResetEvent(false);
             // It will start with 0 seconds delay for 100ms interval
-            _timer = new Timer(Execute, _autoResetEvent, 0, 100);
+            _timer = new Timer(Execute, _autoResetEvent, 0, milliseconds);
             TimerStarted = DateTime.Now;
         }
 
+        /// <summary>
+        /// We are using an Action delegate to execute the passed callback function every X seconds.
+        /// The timer will make a X seconds pause before the first execution.
+        /// Finally, we just create a sixty seconds time slot for execution, to avoid limitless timer loop.
+        /// </summary>
+        /// <param name="stateInfo"></param>
         public void Execute(object stateInfo)
         {
             _action();
-            if ((DateTime.Now - TimerStarted).Seconds > 50)
+            // Run for 10 minutes or stop it manually
+            if ((DateTime.Now - TimerStarted).Seconds > 600)
             {
                 _timer.Dispose();
             }
+        }
+
+        public void Stop()
+        {
+            _timer.Dispose();
         }
     }
 }
