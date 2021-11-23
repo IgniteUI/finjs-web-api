aaba1b133bd1bf17d3ae4e128c0f3f3404d59105
Svetloslav Novoselski <svetli.novoselski@gmail.com>
2021-11-23 16:52:03 +0200

    chore(cors): add infragistics.com to allowed domains

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -26,7 +26,7 @@ namespace WebAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddCors(options =>
+            services.AddCors(options => 
             {
                 options.AddPolicy("CorsPolicy", builder => builder
                 .AllowAnyMethod()
